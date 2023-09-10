be4da00e93d3bcfff851f259923000437e0dc353
L.A.G <130486459+LeoBlute@users.noreply.github.com>
2023-09-10 11:43:56 -0300

    Add files via upload
    Logging now supports multiple arguments at once

--- VUtils/DebugUtils.hpp
+++ VUtils/DebugUtils.hpp
@@ -1,48 +1,38 @@
 #pragma once
-#include <string>
-#include <ctime>
+#include "TimeUtils.hpp"
 #include <iostream>
 #include <type_traits>
 #include <filesystem>
 #include <fstream>
+#include <ostream>
 
 namespace DebugUtils {
-    template <typename T>
-    static void Log(T msg, const char* additional, bool toFile)
+    template<typename T>
+    constexpr static inline void AddToStream
+    (std::ostream& stream, const T& arg)
+    {
+        stream << arg;
+    }
+
+    template<typename T, typename ...Args>
+    constexpr static inline void AddToStream
+    (std::ostream& stream, const T& arg, const Args&... args)
+    {
+        stream << arg;
+        AddToStream(stream, args...);
+    }
+
+    template <typename... Args>
+    static void Log(bool toFile, Args... msg)
     {
         //final message that will appear
         std::string fmsg;
-
         //construct final message beginning text
         fmsg.append("-");
-        {
-        time_t t0 = time(0);
-		char cstr[26];
-		ctime_s(cstr, sizeof(cstr), &t0);
-		fmsg.append(cstr);
-		fmsg.pop_back();
-        }
-        fmsg.append(additional); //type
-
-        //checks if the variable is loggable
-        if constexpr (std::is_arithmetic_v<T>) //if variable is arithmetic
-        {
-            fmsg.append(std::to_string(msg));
-        }
-        else if constexpr (std::is_same_v<T, std::string> || std::is_same_v<T, const char*> || std::is_same_v<T, char*> ||
-            std::is_array_v<T> && std::is_same_v<std::remove_extent_t<T>, const char> ||
-            std::is_array_v<T> && std::is_same_v<std::remove_extent_t<T>, char>) //if variable is not arithmetic but is still loggable
-        {
-            fmsg.append(msg);
-        }
-        else if constexpr (std::is_same_v<T, std::string*>) //if variable is a std::string pointer
-        {
-            fmsg.append(msg->c_str());
-        }
-        else //if variable can not be printed
-        {
-            fmsg.append("Non loggable variable");
-        }
+        TimeUtils::GetDateToStr(fmsg); //date
+        std::ostringstream stream;
+        AddToStream(stream, msg...);
+        fmsg.append(stream.str());
 
         //if toFile is true it will write the log to a new file
         if (toFile)
@@ -51,13 +41,7 @@ namespace DebugUtils {
 
             //Path + file name
             std::string fileName = "Logs\\Log-"; 
-        {
-            time_t t0 = time(0);
-		    char cstr[26];
-		    ctime_s(cstr, sizeof(cstr), &t0);
-		    fileName.append(cstr);
-		    fileName.pop_back();
-        }
+            TimeUtils::GetDateToStr(fileName);
             for (char& c : fileName)
             {
                 if (c == ' ' || c == ':')
@@ -81,39 +65,36 @@ namespace DebugUtils {
 
         //Log final message to console
         std::cout << fmsg << std::endl;
-
     }
 }
 
 #ifdef DEBUG
     //Log to console only
-    #define DEBUG_INFO(MSG) DebugUtils::Log(MSG, "-Info:", false);
-    #define DEBUG_WARN(MSG) DebugUtils::Log(MSG, "-Warning:", false);
-    #define DEBUG_CRIT(MSG) DebugUtils::Log(MSG, "-Critical:", false);
-    #define DEBUG_ERROR(MSG) DebugUtils::Log(MSG, "-Error:", false);\
+    #define DEBUG_INFO(...) DebugUtils::Log(false, "-Info:", __VA_ARGS__);
+    #define DEBUG_WARN(...) DebugUtils::Log(false, "-Warning:", __VA_ARGS__);
+    #define DEBUG_ERROR(...) DebugUtils::Log(false, "-Error:", __VA_ARGS__);\
     __debugbreak();
-    #define DEBUG_TODO(MSG) DebugUtils::Log(MSG, "-TODO:", false);
+    #define DEBUG_TODO(...) DebugUtils::Log(false, "-TODO:", __VA_ARGS__);
     
     //Log to console and write to file
-    #define DEBUG_FILE_INFO(MSG) DebugUtils::Log(MSG, "-Info:", true);
-    #define DEBUG_FILE_WARN(MSG) DebugUtils::Log(MSG, "-Warning:", true);
-    #define DEBUG_FILE_CRIT(MSG) DebugUtils::Log(MSG, "-Critical:", true);
-    #define DEBUG_FILE_ERROR(MSG) DebugUtils::Log(MSG, "-Error:", true);\
+    #define DEBUG_FILE_INFO(...) DebugUtils::Log(true, "-Info:", __VA_ARGS__);
+    #define DEBUG_FILE_WARN(...) DebugUtils::Log(true, "-Warning:", __VA_ARGS__);
+    #define DEBUG_FILE_ERROR(...) DebugUtils::Log(true, "-Erro:", __VA_ARGS__);\
     __debugbreak();
-    #define DEBUG_FILE_TODO(MSG) DebugUtils::Log(MSG, "-TODO:", true);
+    #define DEBUG_FILE_TODO(...) DebugUtils::Log(true, "-TODO:", __VA_ARGS__);
 
 #else
 //Log to console only
-    #define DEBUG_INFO(MSG)
-    #define DEBUG_WARN(MSG)
-    #define DEBUG_CRIT(MSG)
-    #define DEBUG_ERROR(MSG)
-    #define DEBUG_TODO(MSG)
+    #define DEBUG_INFO(...)
+    #define DEBUG_WARN(...)
+    #define DEBUG_CRIT(...)
+    #define DEBUG_ERROR(...)
+    #define DEBUG_TODO(...)
 
 //Log to console and write to file
-    #define DEBUG_FILE_INFO(MSG) 
-    #define DEBUG_FILE_WARN(MSG) 
-    #define DEBUG_FILE_CRIT(MSG)
-    #define DEBUG_FILE_ERROR(MSG)
-    #define DEBUG_FILE_TODO(MSG)
+    #define DEBUG_FILE_INFO(...) 
+    #define DEBUG_FILE_WARN(...) 
+    #define DEBUG_FILE_CRIT(...)
+    #define DEBUG_FILE_ERROR(...)
+    #define DEBUG_FILE_TODO(...)
 #endif // DEBUG
